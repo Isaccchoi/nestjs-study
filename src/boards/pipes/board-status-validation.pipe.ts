c6e7435a59134e582ef6894c190a6510e789ad38
isaccchoi <isaccchoi5@gmail.com>
2022-06-26 17:51:37 +0900

    pipe를 이용한 유효성 체크

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: any): any {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    return this.StatusOptions.indexOf(status) !== -1;
+  }
+}
